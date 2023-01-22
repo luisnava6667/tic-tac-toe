456634cd3f7e36395e473aefddbc72e45eac8a43
luisnava6667 <luisnavarro19890603@gmail.com>
2023-01-22 11:45:59 -0300

    first commit

---
+++ src/logic/board.js
@@ -0,0 +1,17 @@
+import { WINNING_COMBOS } from '../constants';
+export const checkWinnerFrom = (boardToCheck) => {
+   for (const combo of WINNING_COMBOS) {
+      const [a, b, c] = combo;
+      if (
+         boardToCheck[a] &&
+         boardToCheck[a] === boardToCheck[b] &&
+         boardToCheck[a] === boardToCheck[c]
+      ) {
+         return boardToCheck[a];
+      }
+   }
+   return null;
+};
+export const checkEndGame = (newBoard) => {
+   return newBoard.every((square) => square !== null);
+};
---
+++ src/logic/storage/index.js
@@ -0,0 +1,8 @@
+export const saveGameToStorage = ({ board, turn }) => {
+   window.localStorage.setItem('board', JSON.stringify(board));
+   window.localStorage.setItem('turn', turn);
+};
+export const resetGameInStorage = () => {
+   window.localStorage.removeItem('board');
+   window.localStorage.removeItem('turn');
+};
